8b134494dcc39dc03255a1c8490d1aabf83135d1
dat-iro <138441382+dat-iro@users.noreply.github.com>
2023-11-05 12:45:15 +0100

    solution of Components-3

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import HelloWorld from "./HelloWorld";
+import Message from "./Message";
 
 function App() {
   return (
     <div>
       <HelloWorld />
+      <Message /> {/* possiamo fare il render di Message direttamente all'interno del componente App, importando automaticamenete il componenete Message */}
+      <Hello /> {/* si, possiamo usare il componente Hello più volte */}
     </div>
   );
 }
