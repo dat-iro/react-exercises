894d4038efecd51ceb575fc3a14129aa609820d8
dat-iro <138441382+dat-iro@users.noreply.github.com>
2023-11-05 12:38:42 +0100

    solution of Components-2

--- my-app/src/HelloWorld.jsx
+++ my-app/src/HelloWorld.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Message from "./Message";
 
 function HelloWorld() {
-  return <h2>Hello, World!</h2>;
+  return (
+    <div>
+      <h2>Hello, World!</h2>
+      <Message />
+    </div>
+  )
 }
 
 export default HelloWorld;
