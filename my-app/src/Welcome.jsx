2fe15c05dc1b4e24c33584e5519b1d5ab0abec54
Matteo Gatto <138441382+dat-iro@users.noreply.github.com>
2023-11-06 11:28:33 +0100

    Update Welcome.jsx

--- my-app/src/Welcome.jsx
+++ my-app/src/Welcome.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { React } from "react";
 
 export function Welcome({ name = "Franco" }) { /* possiamo assegnare un valore di default a prop name */
   return <p>Welcome, {name}!</p>
