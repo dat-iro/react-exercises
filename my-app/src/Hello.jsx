33dcbf1b6df4176e9cf3d4b71dee96609ebeb15d
Matteo Gatto <138441382+dat-iro@users.noreply.github.com>
2023-11-06 11:27:43 +0100

    Update and rename HelloWorld.jsx to Hello.jsx

---
+++ my-app/src/Hello.jsx
@@ -0,0 +1,11 @@
+import { React } from "react";
+import { Message } from "./Message";
+
+export function Hello() {
+  return (
+    <div>
+      <h2>Hello, World!</h2>
+      <Message />
+    </div>
+  )
+}
