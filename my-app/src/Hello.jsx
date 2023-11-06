04598092e5184ff28e30798c0de3e14bb0445475
Matteo Gatto <138441382+dat-iro@users.noreply.github.com>
2023-11-06 11:15:24 +0100

    Update and rename HelloWorld.jsx to Hello.jsx

---
+++ my-app/src/Hello.jsx
@@ -0,0 +1,5 @@
+import { React } from "react";
+
+export function Hello() {
+  return <h2>Hello, World!</h2>;
+}
--- my-app/src/HelloWorld.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function HelloWorld() {
-  return <h2>Hello, World!</h2>;
-}
-
-export default HelloWorld;
