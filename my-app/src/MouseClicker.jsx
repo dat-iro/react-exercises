2eb13bbf6416ed5549e1a3018835ceab43dabbe0
dat-iro <138441382+dat-iro@users.noreply.github.com>
2023-11-09 12:30:51 +0100

    solution of Handling Events 2

--- my-app/src/MouseClicker.jsx
+++ my-app/src/MouseClicker.jsx
@@ -2,8 +2,12 @@ import { React } from "react";
 
 export function MouseClicker({name="one"}) {
   function handleButtonEvent(event) {
-    console.log(event.target.name)
+    if (event.target.name) {
+      console.log(event.target.name)
+    } else if (event.target.src) {
+      console.log(event.target.src)
+    }
   }
 
-  return <button name={name} onClick={handleButtonEvent}>Clicker</button>
+  return <button name={name} onClick={handleButtonEvent}><Clicker src="https://cdn.britannica.com/16/234216-050-C66F8665/beagle-hound-dog.jpg" width="24px" alt="" />Clicker</button>
 }
