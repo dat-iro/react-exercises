895861977dceed6c8d087190556f07204e025c96
dat-iro <138441382+dat-iro@users.noreply.github.com>
2023-11-13 02:05:44 +0100

    solution

--- my-app/src/Counter.jsx
+++ my-app/src/Counter.jsx
@@ -1,11 +1,19 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import { CounterDisplay } from "./CounterDisplay";
 
-export function Counter(initialCounter = 0, increment = 1, decrement = 1) {
+export function Counter({initialCounter = 0, increment = 1, decrement = 1}) {
   const [counter, setCounter] = useState(initialCounter)
+  const direction = useRef(null)
 
-  useEffect(() => {
-    console.log(counter)
+  useEffect(()=> {
+    let prevdirection = direction.current
+    if (counter > initialCounter) {
+        direction.current = "up"
+    } else if (counter < initialCounter) {
+        direction.current = "down"
+    } 
+
+    if (prevdirection != direction.current) console.log(direction.current)
   }, [counter])
 
   function handleCounterIncrement() {
