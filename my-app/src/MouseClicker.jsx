2c7e4e4aa94f1d1272dca4b7dd40ae6fdde52c0c
dat-iro <138441382+dat-iro@users.noreply.github.com>
2023-11-17 18:31:53 +0100

    solution

--- my-app/src/MouseClicker.jsx
+++ my-app/src/MouseClicker.jsx
@@ -2,12 +2,12 @@ import { React } from "react";
 
 export function MouseClicker({name="one"}) {
   function handleButtonEvent(event) {
-    if (event.target.name) {
-      console.log(event.target.name)
-    } else if (event.target.src) {
-      console.log(event.target.src)
-    }
+    console.log(event.target.name)
   }
 
-  return <button name={name} onClick={handleButtonEvent}><Clicker src="https://cdn.britannica.com/16/234216-050-C66F8665/beagle-hound-dog.jpg" width="24px" alt="" />Clicker</button>
+  function handleImageEvent(event) {
+    console.log(event.currentTarget.src)
+  }
+
+  return <button name={name} onClick={handleButtonEvent}><img onClick={handleImageEvent} src="https://cdn.britannica.com/16/234216-050-C66F8665/beagle-hound-dog.jpg" width="24px" alt="" />Click</button>
 }
