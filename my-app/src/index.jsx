966aa35aabb402c4756d731cc5ed9feb96647651
dat-iro <138441382+dat-iro@users.noreply.github.com>
2023-11-17 23:06:11 +0100

    solution

--- my-app/src/index.jsx
+++ my-app/src/index.jsx
@@ -1,6 +1,7 @@
 import { React, StrictMode } from "react";
 import { createRoot, render } from "react-dom";
 import { App } from "./App";
+import "./index.css"
 
 const root = createRoot(document.getElementById("root"));
 root.render(<StrictMode><App /></StrictMode>);
