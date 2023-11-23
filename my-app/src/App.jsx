afea6e1b5fb3a92dd05e0c56c503f489675f5f42
Matteo Gatto <138441382+dat-iro@users.noreply.github.com>
2023-11-23 15:57:04 +0100

    Update App.jsx

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -2,6 +2,7 @@ import { React } from "react";
 import { Hello } from "./Hello";
 import { Message } from "./Message";
 import { Welcome } from "./Welcome";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 export function App() {
   return (
