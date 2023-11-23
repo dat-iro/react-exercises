9f11547ca6d0b7c3db03a279662532b291ddd62d
dat-iro <138441382+dat-iro@users.noreply.github.com>
2023-11-23 14:45:19 +0100

    solution

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -12,16 +12,10 @@ export function App() {
   }
 
   return (
-    <div>
-      <Hello />
-      <Message /> {/* possiamo fare il render di Message direttamente all'interno del componente App, importando automaticamenete il componenete Message */}
-      <Hello /> {/* si, possiamo usare il componente HelloWorld più volte */}
-      <Welcome name="Matteo" age={23} />
-      <Welcome age={25} /> {/* se nessun prop name viene passato al componente Welcome otterremo undefined */}
-      <Welcome name="John" age={20} />
-      <Welcome />
-      <AlertClock onClick={handleCurrentTime} />
-      <Counter />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/counter" element={<Counter />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
